df298d3a8423f6545e2ff3fc7f5ab02dddd37b3a
hBulldozer <bulldozer@dozer.finance>
2024-05-21 11:58:04 -0300

    allow everything

--- hathor/api_util.py
+++ hathor/api_util.py
@@ -22,17 +22,20 @@ from hathor.transaction.storage import TransactionStorage
 from hathor.transaction.storage.exceptions import TransactionDoesNotExist
 from hathor.util import json_dumpb
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 def set_cors(request: Request, method: str) -> None:
-    request.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-    request.setHeader('Access-Control-Allow-Methods', method)
-    request.setHeader('Access-Control-Allow-Headers', 'x-prototype-version,x-requested-with,content-type')
-    request.setHeader('Access-Control-Max-Age', '604800')
+    request.setHeader("Access-Control-Allow-Origin", "*")
+    request.setHeader("Access-Control-Allow-Methods", method)
+    request.setHeader(
+        "Access-Control-Allow-Headers",
+        "x-prototype-version,x-requested-with,content-type",
+    )
+    request.setHeader("Access-Control-Max-Age", "604800")
 
 
-def render_options(request: Request, verbs: str = 'GET, POST, OPTIONS') -> int:
+def render_options(request: Request, verbs: str = "GET, POST, OPTIONS") -> int:
     """Function to return OPTIONS request.
 
     Most of the APIs only need it for GET, POST and OPTIONS, but verbs can be passed as parameter.
@@ -41,9 +44,10 @@ def render_options(request: Request, verbs: str = 'GET, POST, OPTIONS') -> int:
     :type verbs: str
     """
     from twisted.web import server
+
     set_cors(request, verbs)
-    request.setHeader(b'content-type', b'application/json; charset=utf-8')
-    request.write(b'')
+    request.setHeader(b"content-type", b"application/json; charset=utf-8")
+    request.write(b"")
     request.finish()
     return server.NOT_DONE_YET
 
@@ -54,65 +58,71 @@ def get_missing_params_msg(param_name):
     :param param_name: the missing parameter
     :type param_name: str
     """
-    return json_dumpb({'success': False, 'message': f'Missing parameter: {param_name}'})
+    return json_dumpb({"success": False, "message": f"Missing parameter: {param_name}"})
 
 
-def parse_args(args: dict[bytes, list[bytes]], expected_args: list[str]) -> dict[str, Any]:
-    """Parse all expected arguments. If there are missing arguments, returns the missing arguments
-    """
+def parse_args(
+    args: dict[bytes, list[bytes]], expected_args: list[str]
+) -> dict[str, Any]:
+    """Parse all expected arguments. If there are missing arguments, returns the missing arguments"""
     expected_set = set(expected_args)
     args_set = set()
     for arg1 in args:
-        args_set.add(arg1.decode('utf-8'))
+        args_set.add(arg1.decode("utf-8"))
 
     # if there are expected args missing, we return None
     diff = expected_set.difference(args_set)
     if diff:
-        return {'success': False, 'missing': ', '.join(sorted(diff))}
+        return {"success": False, "missing": ", ".join(sorted(diff))}
 
     ret: dict[str, str] = dict()
     for arg2 in expected_args:
-        key_str = arg2.encode('utf-8')
+        key_str = arg2.encode("utf-8")
         first_param = args[key_str][0]
         assert isinstance(first_param, bytes)
-        ret[arg2] = first_param.decode('utf-8')
+        ret[arg2] = first_param.decode("utf-8")
 
-    return {'success': True, 'args': ret}
+    return {"success": True, "args": ret}
 
 
-def parse_int(raw: Union[str, bytes], *,
-              cap: Optional[int] = None, accept_negative: bool = False, accept_zero: bool = True) -> int:
+def parse_int(
+    raw: Union[str, bytes],
+    *,
+    cap: Optional[int] = None,
+    accept_negative: bool = False,
+    accept_zero: bool = True,
+) -> int:
     """Parse int, by default rejecting negative values."""
     value = int(raw)
     if not accept_zero and value == 0:
-        raise ValueError('zero not accepted')
+        raise ValueError("zero not accepted")
     if not accept_negative and value < 0:
-        raise ValueError('negative value not accepted')
+        raise ValueError("negative value not accepted")
     if cap is not None:
         return min(value, cap)
     return value
 
 
 def validate_tx_hash(hash_hex: str, tx_storage: TransactionStorage) -> tuple[bool, str]:
-    """ Validate if the tx hash is valid and if it exists
-        Return success and a message in case of failure
+    """Validate if the tx hash is valid and if it exists
+    Return success and a message in case of failure
     """
     success = True
-    message = ''
-    pattern = r'[a-fA-F\d]{64}'
+    message = ""
+    pattern = r"[a-fA-F\d]{64}"
     # Check if parameter is a valid hex hash
     if not re.match(pattern, hash_hex):
         success = False
-        message = 'Invalid hash'
+        message = "Invalid hash"
     else:
         try:
             tx_storage.get_transaction(bytes.fromhex(hash_hex))
         except ValueError:
             success = False
-            message = 'Invalid hash format'
+            message = "Invalid hash format"
         except TransactionDoesNotExist:
             success = False
-            message = 'Transaction not found'
+            message = "Transaction not found"
 
     return success, message
 
